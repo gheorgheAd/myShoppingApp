7c805f4ae0b7d898321f878b45ad323a3861b4a2
L.Adina <gheorghelarisa16@yahoo.com>
2022-06-04 11:33:29 +0300

    CartItem Service + Controller + dto

---
+++ src/main/java/com/example/myshoppingapp/service/CartItemService.java
@@ -0,0 +1,8 @@
+package com.example.myshoppingapp.service;
+
+import com.example.myshoppingapp.controller.dto.FinishPurchaseRequest;
+
+public interface CartItemService {
+
+    Integer finishPurchase(FinishPurchaseRequest request);
+}
