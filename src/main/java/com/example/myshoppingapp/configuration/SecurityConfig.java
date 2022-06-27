5aa6d155704deaf82f67dda1f041b658527effd7
Angel Stanciu <102602542+angelstanciu@users.noreply.github.com>
2022-06-27 17:47:44 +0300

    delete comments

--- src/main/java/com/example/myshoppingapp/configuration/SecurityConfig.java
+++ src/main/java/com/example/myshoppingapp/configuration/SecurityConfig.java
@@ -2,21 +2,16 @@ package com.example.myshoppingapp.configuration;
 
 import com.example.myshoppingapp.security.MyUserDetailsService;
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.sql.DataSource;
-
-@Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final DataSource dataSource;
     private final MyUserDetailsService myUserDetailsService;
 
     @Override
@@ -24,16 +19,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .userDetailsService(myUserDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder());
-//                .dataSource(dataSource)
-//                .usersByUsernameQuery("select username, password, enabled from users where username=?")
-//                .authoritiesByUsernameQuery("select username, role from users where username=?");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/register/**","/webjars/**", "/images/**").permitAll()
+                .antMatchers("/", "/register/**", "/webjars/**", "/images/**").permitAll()
                 .antMatchers("/products/**").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/admin").hasRole("ADMIN")// (3)
                 .anyRequest().authenticated() // (4)
